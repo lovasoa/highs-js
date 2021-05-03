0240a2b99ec8385cd8266325b48be2439ba73cc2
lovasoa <pere.jobs@gmail.com>
2021-05-03 15:10:09 +0200

    catch HiGHS aborts to try and provide better errors

--- build/build.sh
+++ build/build.sh
@@ -17,9 +17,8 @@ emmake make -j8 libhighs
 # [-Ox] represents build optimisations (discussed in the next section).
 emcc -O3 \
 	-s EXPORTED_FUNCTIONS="@$(pwd)/exported_functions.json" \
-	-s EXTRA_EXPORTED_RUNTIME_METHODS="['ccall']" \
+	-s EXTRA_EXPORTED_RUNTIME_METHODS="['cwrap']" \
 	-s MODULARIZE=1 \
-	-fexceptions \
 	-flto \
 	--closure 1 \
 	--pre-js "$(pwd)/../src/pre.js" \
--- src/post.js
+++ src/post.js
@@ -1,39 +1,42 @@
 const MODEL_FILENAME = "m.lp";
 
+Module.Highs_readModel = Module.cwrap("Highs_readModel", "number", [
+  "number",
+  "string",
+]);
+Module.Highs_setHighsIntOptionValue = Module.cwrap(
+  "Highs_setHighsIntOptionValue",
+  "number",
+  ["number", "string", "number"]
+);
+Module.Highs_writeSolutionPretty = Module.cwrap(
+  "Highs_writeSolutionPretty",
+  "number",
+  ["number", "string"]
+);
+
 /**
  * Solve a model in the CPLEX LP file format.
  */
 Module["solve"] = function (model_str) {
   FS.writeFile(MODEL_FILENAME, model_str);
   const highs = _Highs_create();
-  const read = Module["ccall"](
-    "Highs_readModel",
-    "number",
-    ["number", "string"],
-    [highs, MODEL_FILENAME]
-  );
   assert_ok(
-    read,
+    () => Module.Highs_readModel(highs, MODEL_FILENAME),
     "read LP model (see http://web.mit.edu/lpsolve/doc/CPLEX-format.htm)"
   );
-  const set_option = Module["ccall"](
-    "Highs_setHighsIntOptionValue",
-    "number",
-    ["number", "string", "number"],
-    [highs, "message_level", 0]
+  assert_ok(
+    () => Module.Highs_setHighsIntOptionValue(highs, "message_level", 0),
+    "set option"
   );
-  assert_ok(set_option, "set option");
-  assert_ok(_Highs_run(highs), "solve the problem");
+  assert_ok(() => _Highs_run(highs), "solve the problem");
   const status = UTF8ToString(
     _Highs_highsModelStatusToChar(highs, _Highs_getModelStatus(highs, 0))
   );
-  const write = Module["ccall"](
-    "Highs_writeSolutionPretty",
-    "number",
-    ["number", "string"],
-    [highs, "/dev/stderr"]
+  assert_ok(
+    () => Module.Highs_writeSolutionPretty(highs, "/dev/stderr"),
+    "write and extract solution"
   );
-  assert_ok(write, "write and extract solution");
   _Highs_destroy(highs);
   const output = parseResult(stderr_lines, status);
   stderr_lines.length = 0;
@@ -62,7 +65,8 @@ function lineToObj(headers, line) {
   const values = lineValues(line);
   const result = {};
   for (let idx = 0; idx < values.length; idx++) {
-    if (idx >= headers.length) throw new Error("Unable to parse solution line: " + line);
+    if (idx >= headers.length)
+      throw new Error("Unable to parse solution line: " + line);
     const value = values[idx];
     const header = headers[idx];
     const parser = known_columns[header];
@@ -73,7 +77,8 @@ function lineToObj(headers, line) {
 }
 
 function parseResult(lines, status) {
-  if (lines.length < 3) throw new Error("Unable to parse solution. Too few lines.");
+  if (lines.length < 3)
+    throw new Error("Unable to parse solution. Too few lines.");
   let headers = lineValues(lines[1]);
   var result = { "Status": status, "Columns": {}, "Rows": [] };
   for (var i = 2; lines[i] != "Rows"; i++) {
@@ -87,6 +92,13 @@ function parseResult(lines, status) {
   return result;
 }
 
-function assert_ok(n, action) {
-  if (n !== 0) throw new Error("Unable to " + action + ". HiGHS error " + n);
+function assert_ok(fn, action) {
+  let err;
+  try {
+    err = fn();
+  } catch (e) {
+    err = e;
+  }
+  if (err !== 0)
+    throw new Error("Unable to " + action + ". HiGHS error " + err);
 }
